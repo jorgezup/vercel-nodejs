85514528728f1eb04b66a6073553c7573bb83126
Jorge Zupirolli <jorgebertolo@gmail.com>
2022-10-11 09:43:18 -0400

    fix: configuration static files working

--- src/server.js
+++ src/server.js
@@ -1,19 +1,28 @@
 // index.js
 const express = require("express");
+const path = require("path");
 
 const app = express();
 const PORT = 4000;
+
+app.use(express.json());
+app.use("/static", express.static(path.join(__dirname, "..", "public")));
 
 app.listen(PORT, () => {
   console.log(`API listening on PORT ${PORT} `);
 });
 
 app.get("/", (req, res) => {
-  res.send("Hey this is my API running 🥳");
+  res.sendFile(path.join(__dirname, "..", "public", "index.html"));
 });
 
 app.get("/about", (req, res) => {
   res.send("This is my about route..... ");
+});
+
+app.post("/register", (req, res) => {
+  const body = req.body;
+  res.status(201).send(body);
 });
 
 // Export the Express API
